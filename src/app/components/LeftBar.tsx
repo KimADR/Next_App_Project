0917a1b3af120824ab99609286127b6e1d9a1cf6
KimADR <andriamahatoky.mael2020@gmail.com>
2024-12-30 19:01:24 +0300

    setting the comment,like,share and save button

--- src/app/components/LeftBar.tsx
+++ src/app/components/LeftBar.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
-import Image from "next/image";
+import Image from "./Image";
+
 
 const menuList = [
     {
@@ -72,7 +73,7 @@ const LeftBar = () => {
         <div className="flex flex-col gap-4 text-lg items-center xxl:items-start">
             {/* LOGO */}
             <Link href="/" className="p-2 rounded-full hover:bg-[#181818]">
-               <Image src="icons/logo.svg" alt="logo" width={24} height={24} />
+               <Image path="icons/logo.svg" alt="logo" w={24} h={24} />
             </Link>
             {/* MENU LIST */}
             <div className="flex flex-col ">
@@ -83,10 +84,10 @@ const LeftBar = () => {
               key={item.id}
             >
               <Image
-                src={`icons/${item.icon}`}
+                path={`icons/${item.icon}`}
                 alt={item.name}
-                width={24}
-                height={24}
+                w={24}
+                h={24}
               />
             <span className="hidden xxl:inline">{item.name}</span>
             </Link>
@@ -94,7 +95,7 @@ const LeftBar = () => {
         </div> 
         {/* BUTTON*/}
         <Link href="/" className="bg-white text-black rounded-full w-10 h-10 flex items-center justify-center xxl:hidden">
-        <Image src="icons/post.svg" alt="new post" width={24} height={24}/>
+        <Image path="icons/post.svg" alt="new post" w={24} h={24}/>
         </Link>
         <Link href="/"
        className="hidden xxl:block bg-white text-black rounded-full font-semibold py-2 px-20"
@@ -106,7 +107,7 @@ const LeftBar = () => {
         <div className="flex items-center justify-between">
         <div className="flex items-center gap-2">
           <div className="w-10 h-10 relative rounded-full overflow-hidden">
-            <Image src="/general/avatar.png" alt="lama dev" fill />
+            <Image path="/general/avatar.png" alt="lama dev" w={100} h={100} tr={true} />
           </div>
           <div className="hidden xxl:flex flex-col">
             <span className="font-bold">Mael Joss</span>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,19 @@
-"use client"
-import { IKImage } from "imagekitio-next";
-
-const urlEndpoint = process.env.NEXT_PUBLIC_URL_ENDPOINT;
-
+import Link from "next/link";
+import Share from "./components/Share";
+import Feed from "./components/Feed";
 
 const Homepage = () => {
-  return (
-    <div className=''>
-      <IKImage
-        urlEndpoint={urlEndpoint}
-        path="general/post.jpeg"
-        width={600}
-        height={600}
-        alt="test post"
-      />
+  return <div className="">
+    <div className='px-4 pt-4 flex justify-between text-textGray font-bold border-b-[1px] border-borderGray'>
+      <Link className="pb-3 flex items-center border-b-4 border-iconBlue" href="/">For you</Link>
+      <Link className="pb-3 flex items-center" href="/">Following</Link>
+      <Link className="pb-3 flex items-center" href="/">React.js</Link>
+      <Link className="pb-3 flex items-center" href="/">Javascript</Link>
+      <Link className="pb-3 flex items-center" href="/">CSS</Link>
     </div>
-  )
-}
+    <Share/>
+    <Feed/>
+  </div>;
+};
 
-export default Homepage
+export default Homepage;
